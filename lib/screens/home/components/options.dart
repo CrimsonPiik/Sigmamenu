a13c190b4c70ad515a09168f243ba622e23bd049
crimsonpiik <moebasem@gmail.com>
2021-12-30 14:18:36 +0200

    fixin

--- lib/screens/home/components/options.dart
+++ lib/screens/home/components/options.dart
@@ -47,6 +47,28 @@ class _OptionsState extends State<Options> {
               setState(() {
                 _menu = !_menu;
               });
+
+              for (var v in widget.product.options['Size'].keys) {
+                // widget.product.options.values [] // BOTH
+                // widget.product.options['Size'].values // 3.50 1.50 2.50
+                // widget.product.options['Size'].keys // Small Meduim Large
+
+                // print(widget.product.options.keys.elementAt(0)); // Size
+                // print(widget.product.options.keys.elementAt(1)); // Add-ins
+
+                /*
+                
+                print(widget.product.options['Size'].values.elementAt(0)); // 1.90
+
+                String vvv = widget.product.options.keys.elementAt(0); //Size
+                print(widget.product.options[vvv].values.elementAt(0)); // Index 0 in Size = 1.90
+                
+                */
+
+                // print(widget.product.options.values.elementAt(0)); // {Large: 2.50, Small: 1.60, Medium: 1.90}
+                // print(widget.product.options.values.elementAt(1)); // {Water: 1.20, Fries: 1.20, Pepsi: 1.20}
+                print(v);
+              }
             },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
